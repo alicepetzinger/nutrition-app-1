55f84ea779368e33a0b1efcd8f1d8e17673243c1
alicepetzinger <alice.petzinger@gmail.com>
2021-10-08 12:40:08 +0200

    add fastfood

---
+++ src/components/fastfood/fastfood.stories.js
@@ -0,0 +1,9 @@
+import "./fastfood.css";
+import fastfood from "./fastfood.html";
+
+export default {
+  title: "Components/Fastfood",
+  parameters: { layout: "centered" },
+};
+
+export const article = () => fastfood;
